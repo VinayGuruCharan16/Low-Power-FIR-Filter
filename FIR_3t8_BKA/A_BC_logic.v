77661c21b4d115a72de9586de51c7f5c1605af03
VinayGuruCharan16 <140330326+VinayGuruCharan16@users.noreply.github.com>
2024-12-10 11:11:39 +0530

    Delete FIR_3t8_BKA directory

--- FIR_3t8_BKA/A_BC_logic.v
+++
@@ -1,5 +0,0 @@
-module A_BC_logic(p, A, B, C);
-input A,B,C;
-output p;
-assign p = A | (B&C);
-endmodule 
--- FIR_3t8_BKA/HA.v
+++
@@ -1,6 +0,0 @@
-module HA (p, g, A, B);
-input A,B;
-output p, g;
-assign p = A ^ B;
-assign g = A & B;
-endmodule 
